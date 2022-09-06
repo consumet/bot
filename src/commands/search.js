388fa70be6296b46ad4302c28dd5ef22f3a42cc6
futaba <mythhb@yahoo.com>
2022-09-05 21:55:19 -0400

    .

--- src/commands/search.js
+++ src/commands/search.js
@@ -3,75 +3,78 @@ const { PROVIDERS_LIST } = require("@consumet/extensions");
 const { capitalizeFirstLetter } = require("@consumet/extensions/dist/utils");
 
 module.exports = {
-        data: new SlashCommandBuilder()
-            .setName("search")
-            .setDescription("Search for a movie/tvshow/anime, etc...")
-            .addStringOption((option) =>
-                option
-                .setName("query")
-                .setDescription("Query to search")
-                .setRequired(true)
+  data: new SlashCommandBuilder()
+    .setName("search")
+    .setDescription("Search for a movie/tvshow/anime, etc...")
+    .addStringOption((option) =>
+      option
+        .setName("query")
+        .setDescription("Query to search")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("provider")
+        .setDescription("Provider to search (default: Anilist)")
+        .setRequired(false)
+        .addChoices(
+          ...Object.keys(PROVIDERS_LIST)
+            .map((c) =>
+              PROVIDERS_LIST[c].map((p) => ({
+                name: capitalizeFirstLetter(p.name),
+                value: p.toString.classPath,
+              }))
             )
-            .addStringOption((option) =>
-                option
-                .setName("provider")
-                .setDescription("Provider to search (default: Anilist)")
-                .setRequired(false)
-                .addChoices(
-                    ...Object.keys(PROVIDERS_LIST)
-                    .map((c) =>
-                        PROVIDERS_LIST[c].map((p) => ({
-                            name: capitalizeFirstLetter(p.name),
-                            value: p.toString.classPath,
-                        }))
-                    )
-                    .flat()
-                    .sort((a, b) => (a.name === b.name ? 0 : a.name < b.name ? -1 : 1))
-                )
-            )
-            .addStringOption((option) =>
-                option
-                .setName("list")
-                .setDescription("set to true to get a list of results")
-                .setRequired(false)
-                .addChoices({ name: "True", value: "true" }, { name: "False", value: "false" })
-            ),
+            .flat()
+            .sort((a, b) => (a.name === b.name ? 0 : a.name < b.name ? -1 : 1))
+        )
+    )
+    .addStringOption((option) =>
+      option
+        .setName("list")
+        .setDescription("set to true to get a list of results")
+        .setRequired(false)
+        .addChoices(
+          { name: "True", value: "true" },
+          { name: "False", value: "false" }
+        )
+    ),
 
-        async execute(interaction) {
-            let category = undefined;
-            if (interaction.options.getString("provider")) {
-                category = interaction.options.getString("provider").split(".")[0];
-            } else category = "META";
+  async execute(interaction) {
+    let category = undefined;
+    if (interaction.options.getString("provider")) {
+      category = interaction.options.getString("provider").split(".")[0];
+    } else category = "META";
 
-            const provider = PROVIDERS_LIST[category].find(
-                (p) =>
-                p.toString.classPath ===
-                (interaction.options.getString("provider") ?
-                    interaction.options.getString("provider") :
-                    "META.Anilist")
-            );
-            const query = interaction.options.getString("query");
+    const provider = PROVIDERS_LIST[category].find(
+      (p) =>
+        p.toString.classPath ===
+        (interaction.options.getString("provider")
+          ? interaction.options.getString("provider")
+          : "META.Anilist")
+    );
+    const query = interaction.options.getString("query");
 
-            let res = undefined;
-            try {
-                res = await provider.search(query);
-            } catch (e) {
-                await interaction.reply({
-                    content: `An error occured while searching for \`${query}\` on \`${provider.toString.name}\``,
-                    ephemeral: true,
-                });
-            }
+    let res = undefined;
+    try {
+      res = await provider.search(query);
+    } catch (e) {
+      await interaction.reply({
+        content: `An error occured while searching for \`${query}\` on \`${provider.toString.name}\``,
+        ephemeral: true,
+      });
+    }
 
-            const isList = interaction.options.getString("list") === "true";
-            if (isList) {
-                res = res.results.slice(0, 4);
-            } else res = [res.results[0]];
+    const isList = interaction.options.getString("list") === "true";
+    if (isList) {
+      res = res.results.slice(0, 4);
+    } else res = [res.results[0]];
 
-            const embeds = [
-                    ...res.map((r) =>
-                        new EmbedBuilder()
-                        .setTitle(
-                            `${
+    const embeds = [
+      ...res.map((r) =>
+        new EmbedBuilder()
+          .setTitle(
+            `${
               typeof r.title !== "string"
                 ? r.title.english
                   ? r.title.english
@@ -129,7 +132,7 @@ module.exports = {
           .setFooter(
             !isList
               ? {
-                  text: `Requested by ${interaction.user.username} | Provided by ${provider.toString.name}`,
+                  text: `Requested by ${interaction.user.username} | Provided by [${provider.toString.name}](${provider.toString.baseUrl})`,
                   iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png`,
                 }
               : null
